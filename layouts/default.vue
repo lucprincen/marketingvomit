1f9499a5f5daef111987a8f665e98bfcad41a8da
Luc Princen <hi@lucp.nl>
2019-06-22 12:27:54 +0200

    links the footer twitter to the actual thread

--- layouts/default.vue
+++ layouts/default.vue
@@ -13,7 +13,7 @@
             <strong>
                 This site is an hommage to horrible marketing jargon. 
             </strong>
-            <p>Disclaimer: this is the dumbest side-project <a href="https://twitter.com/LucP" target="_blank">@LucP</a> has ever created.</p>
+            <p>Disclaimer: this is the dumbest side-project <a href="https://twitter.com/LucP/status/1135816852133863424" target="_blank">@LucP</a> has ever created.</p>
             <small>(also my sincerest appologies to the italian community for hijacking their top-level domain)</small>
         </footer>
     </div>
